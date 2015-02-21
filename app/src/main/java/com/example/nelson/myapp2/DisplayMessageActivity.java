ec72f175f69fd57c3827caf7b00795680b5f4dae
NelsonRock <nelsao101@gmail.com>
2015-02-21 10:50:49 +0100

    Create another activity for get the intend from thr MainActivity

---
+++ app/src/main/java/com/example/nelson/myapp2/DisplayMessageActivity.java
@@ -0,0 +1,25 @@
+package com.example.nelson.myapp2;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class DisplayMessageActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_display_message, menu);
+        return true;
+    }
+}

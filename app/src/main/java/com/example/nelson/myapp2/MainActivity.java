fb946ee0791673d3f53c2a6ea6ae5e6e454a77d5
NelsonRock <nelsao101@gmail.com>
2015-02-23 14:20:38 +0100

    Added ActionBar, new file main_activity_actions.xml

--- app/src/main/java/com/example/nelson/myapp2/MainActivity.java
+++ app/src/main/java/com/example/nelson/myapp2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.nelson.myapp2;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,7 +23,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
         return true;
     }
 
@@ -31,14 +32,27 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        //int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
+        }*/
+
+        switch (item.getItemId()){
+            case R.id.action_search:
+                Log.i("ActionBar", "Search");
+                return true;
+            case R.id.action_settings:
+                Log.i("ActionBar", "Setting");
+                return true;
+            case R.id.action_save:
+                Log.i("ActionBar","Save");
+                return true;
+            default:
+            return super.onOptionsItemSelected(item);
         }
 
-        return super.onOptionsItemSelected(item);
     }
 
 
